b08c547316d32492671915e5144471eb512ad697
Marcin Slupikowski <warszawiak98@gmail.com>
2020-03-26 18:52:50 +0100

    zadanie

--- PAI/zajecia_online/zadanie.php
+++ PAI/zajecia_online/zadanie.php
@@ -1,40 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Zadanie</title>
 </head>
 <body>
-    <?php
-         echo "tekst";
-         $name= "janusz";
-         $surname="kowalski";
-
-            echo $name." ".$surname,"<br/>";
-            echo $name." ".$surname,"<br/>";
-            
-            echo "$name $surname";
-            echo '$name $surname';
-            
-            //heredoc
-echo <<<SHOW
-            <hr>
-                imię: $name<br>
-                Nazwisko: $surname
-            <hr>
-SHOW;
-
-         //heredoc
-         $tekst=<<<SHOW
-         <hr>
-             imię: $name<br>
-             Nazwisko: $surname
-         <hr>
-SHOW;
-
-echo $tekst;
-
-            ?>
+    <div id="top">
+        <header id="NAGLOWEK">
+            <h1 style="font-size:10vw">ZADANKO</h1>
+        </header>
+        
+        
+        <nav id="MENU">Menu nawigacyjne</nav>
+        
+        
+        <aside id="INFORMACJE">
+            <img src="e2.png" style="width:100%;">
+        </aside>
+        
+        
+        <article id="TRESC">
+        <h3 style="font-size:2vw">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</h3></article>
+        
+        
+        
+        <footer id="STOPKA">
+        <h2 style="font-size:5vw">Marcin Słupikowski©</h2>
+        </footer>
+    </div>
 </body>
 </html>
