3b061a8a38bfd12530d4e269b95d186ee9043dc5
Marcin Slupikowski <warszawiak98@gmail.com>
2020-03-26 20:27:58 +0100

    zamiana

--- PAI/zajecia_online/zadanie.php
+++ PAI/zajecia_online/zadanie.php
@@ -4,29 +4,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zadanie</title>
+
+
+    <style>
+        table, td {
+            border: 1.5px solid black;
+            border-collapse: collapse;
+            width: 50%;
+            height: 50%;
+            text-align: center;
+        }
+
+        th{
+            border:0px;
+            border-collapse: collapse;
+            height:20px;
+            padding:0px;
+            border-image-width:0px;
+
+
+        }
+
+ 
+
+</style>
+
 </head>
 <body>
     <div id="top">
         <header id="NAGLOWEK">
-            <h1 style="font-size:10vw">ZADANKO</h1>
+            <h1 style="font-size:2vw">ZADANKO</h1>
         </header>
         
         
-        <nav id="MENU">Menu nawigacyjne</nav>
+        <nav id="MENU">
+                    <h3 style="font-size:1vw">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</h3>
+
+        </nav>
         
         
         <aside id="INFORMACJE">
-            <img src="e2.png" style="width:100%;">
+            <img src="e2.png" style="width:50%;">
         </aside>
         
         
         <article id="TRESC">
-        <h3 style="font-size:2vw">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</h3></article>
+      
+
+<table>
+  <tr>
+    <td rowspan="2">1.1</td>
+    <td >1.2</td>
+  </tr>
+  <tr>
+    <td rowspan="2">2.2</td>
+      <th>
+
+      </th>
+      
+  </tr>
+  <tr>
+    <td>31.</td>
+
+  </tr>
+  <tr>
+    <td colspan="2">4.1</td>
+  </tr>
+</table>
         
-        
-        
+        </article>
+
+
         <footer id="STOPKA">
-        <h2 style="font-size:5vw">Marcin Słupikowski©</h2>
+        <h2 style="font-size:0.8vw">Marcin Słupikowski©</h2>
         </footer>
     </div>
 </body>
